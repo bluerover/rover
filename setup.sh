11f751d40fae14fa4f61b83db926cc79cd65767b
Johann Tutor <jmbtutor@uwaterloo.ca>
2016-11-09 15:53:26 -0500

    Extract virtualenv path to variable
    This path is used multiple times, so it should be put into a variable.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+errbot_venv=".errbot-venv"
+
 die() {
   local exit_code=$?
   [ $exit_code -eq 0 ] && exit_code=1
@@ -13,7 +15,13 @@ command -v python3 >/dev/null 2>&1 || die "python3 not installed."
 # - virtualenv
 command -v virtualenv >/dev/null 2>&1 || die "virtualenv not installed. (pip install virtualenv)"
 
-virtualenv --python "$(which python3)" .errbot-venv || die "Could not create virtual environment."
-./.errbot-venv/bin/pip install errbot || die "Could not install errbot."
+virtualenv --python "$(which python3)" "$errbot_venv" || die "Could not create virtual environment."
 
-ln -s ./.errbot-venv/bin/errbot errbot
+pip() {
+  "${errbot_venv}/bin/pip" "$@"
+}
+
+pip install errbot || die "Could not install errbot."
+pip install slackclient || die "Could not install slackclient."
+
+ln -s "${errbot_venv}/bin/errbot" errbot
